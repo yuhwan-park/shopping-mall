2026b671de2194d11e23d8b9ad7ce051480969df
janghan <bjh4135@gmail.com>
2022-05-23 23:14:25 +0900

    Schema: ProductSchema

---
+++ src/db/schemas/product-schema.js
@@ -0,0 +1,37 @@
+import { Schema } from 'mongoose';
+
+const ProductSchema = new Schema(
+  {
+    category: {
+        type: String,
+        required: true
+    },
+    product_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_image: {
+      type: String,
+      required: true,
+    },
+    product_price: {
+      type: Number,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true
+    }
+
+  },
+  {
+    collection: 'users',
+    timestamps: true,
+  }
+);
+
+export { ProductSchema };
