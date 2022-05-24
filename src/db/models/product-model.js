2a851b1a5844001ac576cec96515bfd7d21d68ea
janghan <bjh4135@gmail.com>
2022-05-24 17:57:35 +0900

    product-model: init

---
+++ src/db/models/product-model.js
@@ -0,0 +1,38 @@
+import { model } from 'mongoose';
+import { ProductSchema } from '../schemas/user-schema';
+
+const User = model('users', ProductSchema);
+
+export class ProductModel {
+  async findByEmail(email) {
+    const user = await User.findOne({ email });
+    return user;
+  }
+
+  async findById(userId) {
+    const user = await User.findOne({ _id: userId });
+    return user;
+  }
+
+  async create(userInfo) {
+    const createdNewUser = await User.create(userInfo);
+    return createdNewUser;
+  }
+
+  async findAll() {
+    const users = await User.find({});
+    return users;
+  }
+
+  async update({ userId, update }) {
+    const filter = { _id: userId };
+    const option = { returnOriginal: false };
+
+    const updatedUser = await User.findOneAndUpdate(filter, update, option);
+    return updatedUser;
+  }
+}
+
+const userModel = new UserModel();
+
+export { userModel };
