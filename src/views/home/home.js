6cc8c45f7c9c8ec462d9ca41da2284e8a8f3306e
sandeulju <tksemf1706@naver.com>
2022-05-26 00:15:41 +0900

    Remove: home.css 삭제 및 home 마크업 수정

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -6,55 +6,93 @@ import * as Api from '/api.js';
 import { randomId } from '/useful-functions.js';
 
 // 요소(element), input 혹은 상수
-const landingDiv = document.querySelector('#landingDiv');
-const greetingDiv = document.querySelector('#greetingDiv');
+// const landingDiv = document.querySelector('#landingDiv');
+// const greetingDiv = document.querySelector('#greetingDiv');
 
-addAllElements();
-addAllEvents();
+// addAllElements();
+// addAllEvents();
 
-// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {
-  insertTextToLanding();
-  insertTextToGreeting();
-}
+// // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+// async function addAllElements() {
+//   insertTextToLanding();
+//   insertTextToGreeting();
+// }
 
-// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-function addAllEvents() {
-  landingDiv.addEventListener('click', alertLandingText);
-  greetingDiv.addEventListener('click', alertGreetingText);
-}
+// // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+// function addAllEvents() {
+//   landingDiv.addEventListener('click', alertLandingText);
+//   greetingDiv.addEventListener('click', alertGreetingText);
+// }
 
-function insertTextToLanding() {
-  landingDiv.insertAdjacentHTML(
-    'beforeend',
-    `
-      <h2>n팀 쇼핑몰의 랜딩 페이지입니다. 자바스크립트 파일에서 삽입되었습니다.</h2>
-    `
-  );
-}
+// function insertTextToLanding() {
+//   landingDiv.insertAdjacentHTML(
+//     'beforeend',
+//     `
+//       <h2>n팀 쇼핑몰의 랜딩 페이지입니다. 자바스크립트 파일에서 삽입되었습니다.</h2>
+//     `
+//   );
+// }
 
-function insertTextToGreeting() {
-  greetingDiv.insertAdjacentHTML(
-    'beforeend',
-    `
-      <h1>반갑습니다! 자바스크립트 파일에서 삽입되었습니다.</h1>
-    `
-  );
-}
+// function insertTextToGreeting() {
+//   greetingDiv.insertAdjacentHTML(
+//     'beforeend',
+//     `
+//       <h1>반갑습니다! 자바스크립트 파일에서 삽입되었습니다.</h1>
+//     `
+//   );
+// }
 
-function alertLandingText() {
-  alert('n팀 쇼핑몰입니다. 안녕하세요.');
-}
+// function alertLandingText() {
+//   alert('n팀 쇼핑몰입니다. 안녕하세요.');
+// }
 
-function alertGreetingText() {
-  alert('n팀 쇼핑몰에 오신 것을 환영합니다');
-}
+// function alertGreetingText() {
+//   alert('n팀 쇼핑몰에 오신 것을 환영합니다');
+// }
 
-async function getDataFromApi() {
-  // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
-  const data = await Api.get('/api/user/data');
-  const random = randomId();
+// async function getDataFromApi() {
+//   // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
+//   const data = await Api.get('/api/user/data');
+//   const random = randomId();
 
-  console.log({ data });
-  console.log({ random });
-}
+//   console.log({ data });
+//   console.log({ random });
+// }
+
+// let curPos = 0;
+// let postion = 0;
+// const IMAGE_WIDTH = 640;
+// const prevBtn = document.querySelector('.prev');
+// const nextBtn = document.querySelector('.next');
+// const images = document.querySelector('.images');
+
+// function prev() {
+//   if (curPos > 0) {
+//     nextBtn.removeAttribute('disabled');
+//     postion += IMAGE_WIDTH;
+//     images.style.transform = `translateX(${postion}px)`;
+//     curPos = curPos - 1;
+//   }
+//   if (curPos == 0) {
+//     prevBtn.setAttribute('disabled', 'true');
+//   }
+// }
+// function next() {
+//   if (curPos < 3) {
+//     prevBtn.removeAttribute('disabled');
+//     postion -= IMAGE_WIDTH;
+//     images.style.transform = `translateX(${postion}px)`;
+//     curPos = curPos + 1;
+//   }
+//   if (curPos == 3) {
+//     nextBtn.setAttribute('disabled', 'true');
+//   }
+// }
+
+// function init() {
+//   prevBtn.setAttribute('disabled', 'true');
+//   prevBtn.addEventListener('click', prev);
+//   nextBtn.addEventListener('click', next);
+// }
+
+// init();
