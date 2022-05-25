49d293aa80f5a2f700c89064a1c98552ec8f857d
yuhwan-park <apkdkvkz@gmail.com>
2022-05-25 23:35:46 +0900

    Style: 주문 조회 페이지 작성

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -21,6 +21,7 @@ viewsRouter.use('/admin', serveStatic('admin'));
 viewsRouter.use('/admin/products', serveStatic('admin/products'));
 viewsRouter.use('/admin/products/:id', serveStatic('admin/products/detail'));
 viewsRouter.use('/admin/add', serveStatic('admin/add'));
+viewsRouter.use('/admin/order_list', serveStatic('admin/order_list'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
