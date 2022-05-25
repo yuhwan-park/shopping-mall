737bed3508cad5ac6d2ebecb568d24e9baac3d26
JoKyungseo <eksql0645@gmail.com>
2022-05-25 21:39:41 +0900

    Docs: 주문생성 수정
    req값 객체로 받아 코드 축약 및 DB저장

--- src/routers/order-router.js
+++ src/routers/order-router.js
@@ -5,7 +5,7 @@ import { orderService } from '../services';
 
 const orderRouter = Router();
 
-// 주문 생성 - 질문
+// 주문 생성
 orderRouter.post('/', async (req, res, next) => {
   try {
     // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
@@ -16,43 +16,11 @@ orderRouter.post('/', async (req, res, next) => {
       );
     }
     // 주문 안에 상품이 여러개일 경우, products를 배열로 받아온다.
-    const {
-      products, // products = [{shortId, quantity}]
-      ordererName,
-      phoneNumber,
-      postalCode,
-      address1,
-      address2,
-      deliveryRequest,
-      deliveryFee,
-      totalPrice,
-    } = req.body.orderInfo;
+    // products = [{shortId, quantity}]
+    const { orderInfo } = req.body;
+
     // jwt token
     const userId = req.currentuserId;
-
-    const address = { postalCode, address1, address2 };
-
-    const orderer = {
-      ordererName,
-      phoneNumber,
-      deliveryRequest,
-    };
-
-    // products에 값이 있을 경우 값 추출
-    // for (i = 0; i <= products.length; i++) {
-    //   const { shortId, quantity } = products[i];
-    //   return { shortId, quantity };
-    // }
-
-    const orderInfo = {
-      id,
-      quantity,
-      userId,
-      orderer,
-      address,
-      deliveryFee,
-      totalPrice,
-    };
 
     // 위 데이터를 주문 db에 추가하기
     const newOrder = await orderService.addOrder(orderInfo);
@@ -63,25 +31,24 @@ orderRouter.post('/', async (req, res, next) => {
   }
 });
 
-// 주문 상세 조회
-orderRouter.get('/:userId/orderId', async function (req, res, next) {
-  try {
-    // 전체 사용자 목록을 얻음
-    const users = await userService.getUsers();
+// // 주문 상세 조회
+// orderRouter.get('/:userId/orderId', async function (req, res, next) {
+//   try {
+//     // 전체 사용자 목록을 얻음
+//     const orders = await orderService.getOrders();
 
-    // 사용자 목록(배열)을 JSON 형태로 프론트에 보냄
-    res.status(200).json(users);
-  } catch (error) {
-    next(error);
-  }
-});
+//     res.status(200).json(orders);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-// 주문 조회
-orderRouter.get(async (req, res, next) => {});
+// // 주문 조회
+// orderRouter.get('/orders/:userId/', async (req, res, next) => {});
 
-// 주문 삭제
-orderRouter.delete(async (req, res, next) => {});
+// // 주문 삭제
+// orderRouter.delete('/:userId/orderId', async (req, res, next) => {});
 
-// 주문 수정
-// orderRouter.fetch(async (req, res, next) => {});
-// export { orderRouter };
+// // 주문 수정
+// // orderRouter.fetch(async (req, res, next) => {});
+// // export { orderRouter };
--- src/services/order-service.js
+++ src/services/order-service.js
@@ -1,4 +1,4 @@
-import { userModel } from '../db';
+import { orderModel } from '../db';
 
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
@@ -10,86 +10,66 @@ class OrderService {
 
   // 주문 생성
   async addOrder(orderInfo) {
-    const {
-      shortId,
-      quantity,
-      userId,
-      orderer,
-      address,
-      deliveryFee,
-      totalPrice,
-    } = orderInfo;
-
-    const newOrderInfo = {
-      shortId,
-      quantity,
-      userId,
-      orderer,
-      address,
-      deliveryFee,
-      totalPrice,
-    };
-
     // db에 저장
-    const createdNewOrder = await this.orderModel.create(newOrderInfo);
+    const createdNewOrder = await this.orderModel.create(orderInfo);
 
     return createdNewOrder;
   }
 
-  // 주문 전체 조회
-  async getOrders() {
-    const orders = await this.orderModel.findAll();
-    return orders;
-  }
+  //   // 주문 전체 조회
+  //   async getOrders() {
+  //     const orders = await this.orderModel.findAll();
+  //     return orders;
+  //   }
 
-  // 주문 수정 - 추가, 수량변경
-  async setOrder(orderInfoRequired, toUpdate) {
-    // 객체 destructuring
-    const { quantity, name, address, currentPassword } = orderInfoRequired;
+  //   // 주문 수정 - 추가, 수량변경
+  //   async setOrder(orderInfoRequired, toUpdate) {
+  //     // 객체 destructuring
+  //     const { quantity, name, address, currentPassword } = orderInfoRequired;
 
-    // 우선 해당 id의 주문이 db에 있는지 확인
-    let order = await this.orderModel.findById(orderId);
+  //     // 우선 해당 id의 주문이 db에 있는지 확인
+  //     let order = await this.orderModel.findById(orderId);
 
-    // db에서 찾지 못한 경우, 에러 메시지 반환
-    if (!order) {
-      throw new Error('주문 내역이 없습니다. 다시 한 번 확인해 주세요.');
-    }
+  //     // db에서 찾지 못한 경우, 에러 메시지 반환
+  //     if (!order) {
+  //       throw new Error('주문 내역이 없습니다. 다시 한 번 확인해 주세요.');
+  //     }
 
-    // 이제, 정보 수정을 위해 사용자가 입력한 비밀번호가 올바른 값인지 확인해야 함
+  //     // 이제, 정보 수정을 위해 사용자가 입력한 비밀번호가 올바른 값인지 확인해야 함
 
-    // 비밀번호 일치 여부 확인
-    const correctPasswordHash = user.password;
-    const isPasswordCorrect = await bcrypt.compare(
-      currentPassword,
-      correctPasswordHash,
-    );
+  //     // 비밀번호 일치 여부 확인
+  //     const correctPasswordHash = user.password;
+  //     const isPasswordCorrect = await bcrypt.compare(
+  //       currentPassword,
+  //       correctPasswordHash,
+  //     );
 
-    if (!isPasswordCorrect) {
-      throw new Error(
-        '현재 비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.',
-      );
-    }
+  //     if (!isPasswordCorrect) {
+  //       throw new Error(
+  //         '현재 비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.',
+  //       );
+  //     }
 
-    // 이제 드디어 업데이트 시작
+  //     // 이제 드디어 업데이트 시작
 
-    // 비밀번호도 변경하는 경우에는, 회원가입 때처럼 해쉬화 해주어야 함.
-    const { password } = toUpdate;
+  //     // 비밀번호도 변경하는 경우에는, 회원가입 때처럼 해쉬화 해주어야 함.
+  //     const { password } = toUpdate;
 
-    if (password) {
-      const newPasswordHash = await bcrypt.hash(password, 10);
-      toUpdate.password = newPasswordHash;
-    }
+  //     if (password) {
+  //       const newPasswordHash = await bcrypt.hash(password, 10);
+  //       toUpdate.password = newPasswordHash;
+  //     }
 
-    // 업데이트 진행
-    user = await this.userModel.update({
-      userId,
-      update: toUpdate,
-    });
+  //     // 업데이트 진행
+  //     user = await this.userModel.update({
+  //       userId,
+  //       update: toUpdate,
+  //     });
 
-    return user;
-  }
+  //     return user;
+  //   }
 }
 
-const userService = new UserService(userModel);
+const orderService = new OrderService(orderModel);
 
-export { userService };
+export { orderService };
