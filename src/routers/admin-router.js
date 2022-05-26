d7ea10dc3a67dca750b82b55b58b38667e1dcdca
janghan <bjh4135@gmail.com>
2022-05-26 15:47:27 +0900

    Feat: admin router init

--- src/routers/admin-router.js
+++ src/routers/admin-router.js
@@ -2,15 +2,36 @@ import { Router } from 'express';
 import is from '@sindresorhus/is';
 
 import { adminRequired } from '../middlewares';
+import { productService } from '../services';
 
 const adminRouter = Router();
 
-adminRouter.get('/', adminRequired, (req, res) => {
+adminRouter.get('/', adminRequired, async (req, res) => {
   res.status(200).json({
-    result: 'approach-success'
-  })
+    result: 'approach-success',
+  });
 });
 
-adminRouter.post('/')
+adminRouter.post('/products', async (req, res) => {
+  const {
+    category,
+    brand,
+    name,
+    shortDescription,
+    detailDescription,
+    imageURL,
+    price,
+  } = req.body;
+  const newproduct = await productService.addProduct({
+    category,
+    brand,
+    name,
+    shortDescription,
+    detailDescription,
+    imageURL,
+    price,
+  });
+  res.status(200).json(newproduct);
+});
 
 export { adminRouter };
