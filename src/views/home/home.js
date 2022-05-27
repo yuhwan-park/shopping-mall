49f689cc5e1a6f135c7f1cf618a8e8d299575e36
sandeulju <tksemf1706@naver.com>
2022-05-27 23:11:56 +0900

    Rename: image slide 작업(진행중)

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -17,6 +17,7 @@ $slider.style.width = sliderWidth * totalSlides + 'px';
 function showSlides() {
   for (let i = 0; i < $slides.length; i++) {
     // sliderWidth.style.left = -(sliderWidth * slideIndex) + 'px';
+
     sliderWidth.style.transform = `translateX(-${sliderWidth * slideIndex}px)`;
   }
   slideIndex++;
