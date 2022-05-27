e4432b07a8d3ab04e2c53d9bf771f4a82769ac11
minsun <judy.leems@gmail.com>
2022-05-27 14:49:21 +0900

    Feat: 회원정보 수정 Api.get 적용

--- src/views/account/security/security.js
+++ src/views/account/security/security.js
@@ -1,27 +1,19 @@
 import * as Api from '/api.js';
 
 // 요소(element), input 혹은 상수
-const userEmailText = document.querySelector('#userEmailText');
-const fullNameInput = document.querySelector('#fullNameInput');
-const passwordInput = document.querySelector('#passwordInput');
-const passwordConfirmInput = document.querySelector('#passwordConfirmInput');
-const addressInput = document.querySelector('#addressInput');
-const phoneInput = document.querySelector('#phoneInput');
-const currentPasswordConfirmInput = document.querySelector(
+const $userEmailText = document.querySelector('#userEmailText');
+const $fullNameInput = document.querySelector('#fullNameInput');
+const $passwordInput = document.querySelector('#passwordInput');
+const $passwordConfirmInput = document.querySelector('#passwordConfirmInput');
+const $addressInput = document.querySelector('#addressInput');
+const $phoneInput = document.querySelector('#phoneInput');
+const $currentPasswordConfirmInput = document.querySelector(
   '#currentPasswordConfirmInput',
 );
-const swithCheckboxs = document.querySelectorAll('.switch');
-const modalPassword = document.querySelector('#modal-js-password');
-const submitButton = document.querySelector('#submitButton');
-const submitConfirmButton = document.querySelector('#submitConfirmButton');
-
-// validator
-let fullName = fullNameInput.value;
-let password = passwordInput.value;
-let passwordConfirm = passwordConfirmInput.value;
-let address = addressInput.value;
-let phone = phoneInput.value;
-const data = { fullName, password, address, phone };
+const $swithCheckboxs = document.querySelectorAll('.switch');
+const $modalPassword = document.querySelector('#modal-js-password');
+const $submitButton = document.querySelector('#submitButton');
+const $submitConfirmButton = document.querySelector('#submitConfirmButton');
 
 addAllElements();
 addAllEvents();
@@ -32,23 +24,22 @@ async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  swithCheckboxs.forEach((swithCheckbox) => {
+  $swithCheckboxs.forEach((swithCheckbox) => {
     const checkbox = swithCheckbox.querySelector('input');
     checkbox.addEventListener('change', handleSwitch);
   });
-  submitButton.addEventListener('click', handleSubmit);
-  submitConfirmButton.addEventListener('click', handlePasswordSubmit);
-  currentPasswordConfirmInput.addEventListener('click', handleUserSubmit);
+  $submitButton.addEventListener('click', handleSubmit);
+  $submitConfirmButton.addEventListener('click', handleUserSubmit);
 }
 
 async function userData() {
   try {
     const data = await Api.get('/api/users/:shortId');
-
-    fullName = data.fullName;
-    userEmailText.insertAdjacentHTML('beforeend', ` (${data.email})`);
-
-    console.log(fullName);
+    $userEmailText.insertAdjacentHTML('beforeend', ` (${data.email})`);
+    $fullNameInput.value = data.fullName;
+    $phoneInput.value = data.phone;
+    $addressInput.value = data.address;
+    console.log(data);
   } catch (err) {
     console.error(err);
   }
@@ -77,26 +68,34 @@ async function handleSwitch(e) {
 async function handleSubmit(e) {
   e.preventDefault();
 
+  const fullName = $fullNameInput.value;
+  const password = $passwordInput.value;
+  const passwordConfirm = $passwordConfirmInput.value;
+  const phone = $phoneInput.value;
+
   // 잘 입력했는지 확인
   const isFullNameValid = fullName.length >= 2;
   const isPasswordValid = password.length >= 4;
   const isPasswordSame = password === passwordConfirm;
   const isPhoneValid = phone.length > 12;
 
-  if (!isFullNameValid || !isPasswordValid) {
-    return alert('이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.');
+  if (!isFullNameValid) {
+    return alert('이름은 2글자 이상이어야 합니다.');
   }
 
-  if (!isPasswordSame) {
+  if (password.length > 0 && isPasswordValid) {
+    return alert('비밀번호는 4글자 이상이어야 합니다.');
+  }
+  if (password.length > 0 && !isPasswordSame) {
     return alert('비밀번호가 일치하지 않습니다.');
   }
 
-  if (!isPhoneValid) {
+  if (phone.length > 0 && !isPhoneValid) {
     return alert('전화번호 형식이 맞지 않습니다.');
   }
 
   try {
-    modalPassword.classList.add('is-active');
+    $modalPassword.classList.add('is-active');
   } catch (err) {
     console.error(err);
   }
@@ -104,7 +103,10 @@ async function handleSubmit(e) {
 
 async function handleUserSubmit(e) {
   e.preventDefault();
-
-  //const user = await Api.patch('/api/users/:shortId', data);
-  console.log(data);
+  const fullName = $fullNameInput.value;
+  const password = $passwordInput.value;
+  const passwordConfirm = $passwordConfirmInput.value;
+  const phone = $phoneInput.value;
+  const data = { fullName, password, passwordConfirm, phone };
+  const user = await Api.patch('/api/users/:shortId', data);
 }
