9141b6c49be8504a87c13d4cf74215df929759b0
sandeulju <tksemf1706@naver.com>
2022-05-27 16:00:12 +0900

    Feat: ImageSlide 기능 구현(진행중)

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,98 +1,37 @@
-// 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
-// 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
-// 코드 예시를 남겨 두었습니다.
+let curPos = 0;
+let postion = 0;
+const IMAGE_WIDTH = 1000;
+const $prevBtn = document.querySelector('.prev');
+const $nextBtn = document.querySelector('.next');
+const $images = document.querySelector('.home-slideshow');
 
-import * as Api from '/api.js';
-import { randomId } from '/useful-functions.js';
+function prev() {
+  if (curPos > 0) {
+    $nextBtn.removeAttribute('disabled');
+    postion += IMAGE_WIDTH;
+    $images.style.transform = `translateX(${postion}px)`;
+    curPos = curPos - 1;
+  }
+  if (curPos == 0) {
+    $prevBtn.setAttribute('disabled', 'true');
+  }
+}
+function next() {
+  if (curPos < 1) {
+    $prevBtn.removeAttribute('disabled');
+    postion -= IMAGE_WIDTH;
+    $images.style.transform = `translateX(${postion}px)`;
+    curPos = curPos + 1;
+  }
+  if (curPos == 1) {
+    $nextBtn.setAttribute('disabled', 'true');
+  }
+}
 
-// 요소(element), input 혹은 상수
-// const landingDiv = document.querySelector('#landingDiv');
-// const greetingDiv = document.querySelector('#greetingDiv');
+function buttonSlide() {
+  $prevBtn.setAttribute('disabled', 'true');
+  $prevBtn.addEventListener('click', prev);
+  $nextBtn.addEventListener('click', next);
+}
 
-// addAllElements();
-// addAllEvents();
-
-// // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-// async function addAllElements() {
-//   insertTextToLanding();
-//   insertTextToGreeting();
-// }
-
-// // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-// function addAllEvents() {
-//   landingDiv.addEventListener('click', alertLandingText);
-//   greetingDiv.addEventListener('click', alertGreetingText);
-// }
-
-// function insertTextToLanding() {
-//   landingDiv.insertAdjacentHTML(
-//     'beforeend',
-//     `
-//       <h2>n팀 쇼핑몰의 랜딩 페이지입니다. 자바스크립트 파일에서 삽입되었습니다.</h2>
-//     `
-//   );
-// }
-
-// function insertTextToGreeting() {
-//   greetingDiv.insertAdjacentHTML(
-//     'beforeend',
-//     `
-//       <h1>반갑습니다! 자바스크립트 파일에서 삽입되었습니다.</h1>
-//     `
-//   );
-// }
-
-// function alertLandingText() {
-//   alert('n팀 쇼핑몰입니다. 안녕하세요.');
-// }
-
-// function alertGreetingText() {
-//   alert('n팀 쇼핑몰에 오신 것을 환영합니다');
-// }
-
-// async function getDataFromApi() {
-//   // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
-//   const data = await Api.get('/api/user/data');
-//   const random = randomId();
-
-//   console.log({ data });
-//   console.log({ random });
-// }
-
-// let curPos = 0;
-// let postion = 0;
-// const IMAGE_WIDTH = 640;
-// const prevBtn = document.querySelector('.prev');
-// const nextBtn = document.querySelector('.next');
-// const images = document.querySelector('.images');
-
-// function prev() {
-//   if (curPos > 0) {
-//     nextBtn.removeAttribute('disabled');
-//     postion += IMAGE_WIDTH;
-//     images.style.transform = `translateX(${postion}px)`;
-//     curPos = curPos - 1;
-//   }
-//   if (curPos == 0) {
-//     prevBtn.setAttribute('disabled', 'true');
-//   }
-// }
-// function next() {
-//   if (curPos < 3) {
-//     prevBtn.removeAttribute('disabled');
-//     postion -= IMAGE_WIDTH;
-//     images.style.transform = `translateX(${postion}px)`;
-//     curPos = curPos + 1;
-//   }
-//   if (curPos == 3) {
-//     nextBtn.setAttribute('disabled', 'true');
-//   }
-// }
-
-// function init() {
-//   prevBtn.setAttribute('disabled', 'true');
-//   prevBtn.addEventListener('click', prev);
-//   nextBtn.addEventListener('click', next);
-// }
-
-// init();
+buttonSlide();
