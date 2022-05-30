e3dd28d1311c922841bc5d3fd8685c490d7657b5
minsun <judy.leems@gmail.com>
2022-05-30 15:46:57 +0900

    Feat: signout 작업 중

--- src/views/account/signout/signout.js
+++ src/views/account/signout/signout.js
@@ -2,8 +2,11 @@ import * as Api from '/api.js';
 import { INPUT } from '/useful-validator.js';
 
 // 요소(element), input 혹은 상수
-const $submitButton = document.querySelector('#submitButton');
+const SECRET_KEY = 'MY-SECRET-KEY';
 const $passwordInput = document.querySelector('#passwordInput');
+const $submitButton = document.querySelector('#submitButton');
+const $deleteButton = document.querySelector('#deleteButton');
+const $modalSignout = document.querySelector('#modal-js-signout');
 
 addAllElements();
 addAllEvents();
@@ -14,21 +17,41 @@ async function addAllElements() {}
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
   $submitButton.addEventListener('click', handleSubmit);
+  $deleteButton.addEventListener('click', handleUserDelete);
 }
 
-function handleSubmit() {
+async function handleSubmit(e) {
+  e.preventDefault();
+
   const password = $passwordInput.value;
   const isPasswordValid = password.length >= INPUT.MINLENGTH.password;
 
-  if (password.length > 0 && !isPasswordValid) {
+  if (!isPasswordValid) {
     return alert('비밀번호는 4글자 이상이어야 합니다.');
-  }
-
-  if (password.length > 0 && !isPasswordSame) {
-    return alert('비밀번호가 일치하지 않습니다.');
   }
 
   if (password === 'kakao') {
     return alert('kakao');
+  }
+
+  try {
+    const data = { password };
+    await Api.post('/api/users', data);
+    $modalSignout.classList.add('is-modal');
+  } catch (err) {
+    alert(`${err.message}`);
+  }
+}
+
+async function handleUserDelete(e) {
+  e.preventDefault;
+
+  try {
+    await Api.delete('/api/users');
+    localStorage.removeItem('key');
+    window.location.href = '/home';
+  } catch (err) {
+    console.error(err);
+    alert(`${err.message}`);
   }
 }
