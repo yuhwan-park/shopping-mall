8c7732ee8519c770ad57a7f5cc4676f24144f74c
JoKyungseo <eksql0645@gmail.com>
2022-05-30 18:57:52 +0900

    Feat: 주문 생성 시 사용자 주소 업데이트

--- src/db/models/order-model.js
+++ src/db/models/order-model.js
@@ -9,22 +9,29 @@ export class OrderModel {
   // 주문 생성
   async create(orderInfo) {
     // product 정보 가져오기 - shortId를 사용해서 ObjectId를 가져온다!
-    const { products } = orderInfo;
-    // products = [{shortId, quantity}]
-    // products에 값이 있을 경우 값 추출
+    const { products, userId } = orderInfo;
+
+    // products = [{shortId, quantity}] - products에 값이 있을 경우 값 추출
     for (let i = 0; i < products.length; i++) {
       const { shortId } = products[i];
+
       // productModel에서 shortId로 검색
       const product = await productModel.findById(shortId);
-      delete products[i].shortId;
-      products[i].productId = product._id;
-      // products = [{productId, quantity}]
-    }
 
+      delete products[i].shortId;
+      products[i].productId = product._id; // products = [{productId, quantity}]
+    }
     orderInfo.products = products;
+
     // orderDB에 데이터 추가
     const createdNewOrder = await Order.create(orderInfo);
 
+    // 사용자 주소를 입력한 주소로 업데이트
+    const user = await User.findById(userId);
+    const updateUser = await User.findOneAndUpdate(
+      { _id: userId },
+      { address: createdNewOrder.address },
+    );
     return createdNewOrder;
   }
 
