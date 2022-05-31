4a45fcc83befe5a34d65e6a731e46c9ba32ca2cc
sandeulju <tksemf1706@naver.com>
2022-05-31 23:17:39 +0900

    Fix: 코드리뷰 반영

--- src/views/product/detail/detail.js
+++ src/views/product/detail/detail.js
@@ -9,7 +9,6 @@ const $detailContents = document.querySelector('.p-contents');
 
 const path = window.location.pathname.split('/');
 const id = path[path.length - 2];
-console.log(id);
 
 // 1. 저장되어 있는 상품의 정보를 불러와서 화면에 텍스트로 출력(스플래시/아이보리니트/가격/설명)
 // 2. db에 저장된 이미지를 불러와서 화면에 출력
@@ -20,7 +19,7 @@ console.log(id);
 //  - 2 로그인이 되어있을 때 = 주문결제 페이지로 이동
 // (결제 정보에 해당 상품에 대한 정보 출력)
 
-// 5.(아마도 완료???) viewrouter에 쿼리나 파라미터 추가(몇번째/무슨 상품인지 식별)
+// 5. viewrouter에 쿼리나 파라미터 추가(몇번째/무슨 상품인지 식별)
 
 async function addAllElements() {
   detailText();
@@ -41,7 +40,6 @@ const token = localStorage.getItem('token');
 
 async function detailText() {
   const data = await Api.get('/api/products', id);
-  console.log(data);
   const node = `<img
             id="detailProductImage"
             src="${data.imageURL}"
@@ -78,8 +76,6 @@ async function detailText() {
 
 // 장바구니에 추가 버튼 함수
 async function addCart(data) {
-  // const order = await Api.get('/api/orders', shortId);
-
   const oldData = JSON.parse(localStorage.getItem('cart'));
   const oldOrderData = JSON.parse(localStorage.getItem('order'));
   const orderData = {
@@ -90,25 +86,24 @@ async function addCart(data) {
       : data.price,
     selectedIds: oldOrderData ? [...oldOrderData['ids'], id] : [id],
   };
-  console.log(oldData);
+
   if (oldData) {
     // 축적된 데이터가 있으면
     localStorage.setItem('cart', JSON.stringify([...oldData, data]));
-    localStorage.setItem('order', JSON.stringify(orderData));
     // order에서 shortsId 가져와서 더해줌
     // productsCount = ids.length
     // selectedIds === ids
   } else {
     // 없으면 (처음으로 장바구니에 물건을 담는다면)
     localStorage.setItem('cart', JSON.stringify([data]));
-    localStorage.setItem('order', JSON.stringify(orderData));
   }
+  localStorage.setItem('order', JSON.stringify(orderData));
 }
 
 function purchase(data) {
   if (!token || token === 'null') {
     // 로그인이 안되어있다면
-    window.location.href = `/login`;
+    window.location.href = '/login';
   } else {
     // 주문결제 페이지로 데이터 전송
     const purchaseData = {
@@ -120,7 +115,7 @@ function purchase(data) {
 
     localStorage.setItem('order', JSON.stringify(purchaseData));
 
-    window.location.href = `/order`;
+    window.location.href = '/order';
   }
 }
 
