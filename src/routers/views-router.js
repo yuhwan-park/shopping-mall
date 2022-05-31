ed99428f1eb028f09085d1aa56da749952a52102
sandeulju <tksemf1706@naver.com>
2022-05-31 16:49:17 +0900

    Feat: 상품 view 페이지 기능 구현

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -23,7 +23,7 @@ viewsRouter.use('/admin/products/:id', serveStatic('admin/products/detail'));
 viewsRouter.use('/admin/add', serveStatic('admin/add'));
 viewsRouter.use('/admin/order_list', serveStatic('admin/order_list'));
 viewsRouter.use('/product/list', serveStatic('product'));
-viewsRouter.use('/product/detail/:id', serveStatic('product/detail'));
+viewsRouter.use('/products/detail/:id', serveStatic('product/detail'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.jsgit  등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
