727374f17aeccd87f8187fb7ad68b06af1bf8804
janghan <bjh4135@gmail.com>
2022-05-31 17:09:01 +0900

    Feat: paginatioin ver. 1

---
+++ src/middlewares/pagination.js
@@ -0,0 +1,10 @@
+async function pagination(total, page, perPage) {
+  const sorted = await total.sort((a, b) => b.updatedAt - a.updatedAt);
+  const startIndex = (page - 1) * perPage;
+  const pageLimit = page * perPage;
+  const endIndex = sorted.length > pageLimit ? pageLimit : sorted.length;
+  const posts = await sorted.slice(startIndex, endIndex);
+  return posts;
+}
+
+export { pagination };
