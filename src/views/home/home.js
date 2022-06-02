50af96a4121928183bfad381b342761de752d8f1
yuhwan-park <apkdkvkz@gmail.com>
2022-06-02 13:23:31 +0900

    Feat: 카테고리 정보로 홈 화면 구성하는 기능, 슬라이드 버그 수정

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,31 +1,35 @@
+import * as Api from '/api.js';
+
 let curPos = 0;
 let postion = 0;
 let imageWidth = 1000;
 let slideIndex = 0;
+let timeOut;
 
 const $prevButton = document.querySelector('.prev');
 const $nextButton = document.querySelector('.next');
 
-const $slideWrapper = document.querySelector('.home-slideshow');
-const $slider = document.querySelector('.slideshow');
-const $slides = document.querySelectorAll('.slide');
-
-const totalSlides = $slides.length;
-let sliderWidth = $slideWrapper.clientWidth;
-$slider.style.width = sliderWidth * totalSlides + 'px';
+const $slideShowContainer = document.querySelector('#slideShowContainer');
 
 // 자동 슬라이드 함수
 function showSlides() {
+  const $slideWrapper = document.querySelector('.home-slideshow');
+  const $slides = document.querySelectorAll('.slide');
+  const totalSlides = $slides.length;
+  let sliderWidth = $slideWrapper.clientWidth;
+  $slideShowContainer.style.width = sliderWidth * totalSlides + 'px';
   for (let i = 0; i < $slides.length; i++) {
-    // sliderWidth.style.left = -(sliderWidth * slideIndex) + 'px';
-
-    $slider.style.transform = `translateX(-${sliderWidth * slideIndex}px)`;
+    $slideShowContainer.style.transform = `translateX(-${
+      sliderWidth * slideIndex
+    }px)`;
   }
   slideIndex++;
+  next();
   if (slideIndex === totalSlides) {
     slideIndex = 0;
+    prev();
   }
-  setTimeout(showSlides, 4000);
+  timeOut = setTimeout(showSlides, 4000);
 }
 
 // 수동 슬라이드 (버튼)
@@ -33,7 +37,7 @@ function prev() {
   if (curPos > 0) {
     $nextButton.removeAttribute('disabled');
     postion += imageWidth;
-    $slider.style.transform = `translateX(${postion}px)`;
+    $slideShowContainer.style.transform = `translateX(${postion}px)`;
     curPos = curPos - 1;
   }
   if (curPos == 0) {
@@ -45,7 +49,7 @@ function next() {
   if (curPos < 1) {
     $prevButton.removeAttribute('disabled');
     postion -= imageWidth;
-    $slider.style.transform = `translateX(${postion}px)`;
+    $slideShowContainer.style.transform = `translateX(${postion}px)`;
     curPos = curPos + 1;
   }
   if (curPos == 1) {
@@ -57,7 +61,37 @@ function buttonSlide() {
   $prevButton.setAttribute('disabled', 'true');
   $prevButton.addEventListener('click', prev);
   $nextButton.addEventListener('click', next);
+  $slideShowContainer.addEventListener('mouseover', () => {
+    clearTimeout(timeOut);
+  });
+  $slideShowContainer.addEventListener('mouseleave', () => {
+    timeOut = setTimeout(showSlides, 4000);
+  });
 }
 
+getCategories();
 buttonSlide();
-showSlides();
+
+async function getCategories() {
+  const categories = await Api.get('/api/admin', 'categories');
+  const slides = categories.reduce(
+    (acc, category) =>
+      (acc += `
+    <div class="slide">
+      <div class="slide-textbox">
+        <span>${category.name}</span>
+        <p>${category.content}</p>
+      </div>
+      <a href="/product/list?category=${category.shortId}">
+        <img
+          src=${category.imageURL}
+          alt=${category.name}
+        />
+      </a>
+    </div>
+  `),
+    '',
+  );
+  $slideShowContainer.insertAdjacentHTML('afterbegin', slides);
+  showSlides();
+}
