c55408f6405cc9197232a6ae8f33952da3413d30
yuhwan-park <apkdkvkz@gmail.com>
2022-06-02 19:07:36 +0900

    Feat: 카테고리 조회 후 슬라이드 구성

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,84 +1,33 @@
 import * as Api from '/api.js';
 import { addCommas } from '/useful-functions.js';
 
-let curPos = 0;
-let postion = 0;
-let imageWidth = 1000;
-let slideIndex = 0;
-let timeOut;
+const $swiperWrapper = document.querySelector('#swiperWrapper');
 
-const $prevButton = document.querySelector('.prev');
-const $nextButton = document.querySelector('.next');
-
-const $slideShowContainer = document.querySelector('#slideShowContainer');
-
-// 자동 슬라이드 함수
-function showSlides() {
-  const $slideWrapper = document.querySelector('.home-slideshow');
-  const $slides = document.querySelectorAll('.slide');
-  const totalSlides = $slides.length;
-  let sliderWidth = $slideWrapper.clientWidth;
-  $slideShowContainer.style.width = sliderWidth * totalSlides + 'px';
-  for (let i = 0; i < $slides.length; i++) {
-    $slideShowContainer.style.transform = `translateX(-${
-      sliderWidth * slideIndex
-    }px)`;
-  }
-  slideIndex++;
-  next();
-  if (slideIndex === totalSlides) {
-    slideIndex = 0;
-    prev();
-  }
-  timeOut = setTimeout(showSlides, 4000);
-}
-
-// 수동 슬라이드 (버튼)
-function prev() {
-  if (curPos > 0) {
-    $nextButton.removeAttribute('disabled');
-    postion += imageWidth;
-    $slideShowContainer.style.transform = `translateX(${postion}px)`;
-    curPos = curPos - 1;
-  }
-  if (curPos == 0) {
-    $prevButton.setAttribute('disabled', 'true');
-  }
-}
-
-function next() {
-  if (curPos < 1) {
-    $prevButton.removeAttribute('disabled');
-    postion -= imageWidth;
-    $slideShowContainer.style.transform = `translateX(${postion}px)`;
-    curPos = curPos + 1;
-  }
-  if (curPos == 1) {
-    $nextButton.setAttribute('disabled', 'true');
-  }
-}
-
-function buttonSlide() {
-  $prevButton.setAttribute('disabled', 'true');
-  $prevButton.addEventListener('click', prev);
-  $nextButton.addEventListener('click', next);
-  $slideShowContainer.addEventListener('mouseover', () => {
-    clearTimeout(timeOut);
-  });
-  $slideShowContainer.addEventListener('mouseleave', () => {
-    timeOut = setTimeout(showSlides, 4000);
-  });
-}
+var swiper = new Swiper('.mySwiper', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 getCategories();
-buttonSlide();
 
 async function getCategories() {
   const categories = await Api.get('/api/admin', 'categories');
   const slides = categories.reduce(
     (acc, category) =>
       (acc += `
-    <div class="slide">
+    <div class="swiper-slide">
       <div class="slide-textbox">
         <span>${category.name}</span>
         <p>${category.content}</p>
@@ -93,8 +42,7 @@ async function getCategories() {
   `),
     '',
   );
-  $slideShowContainer.insertAdjacentHTML('afterbegin', slides);
-  showSlides();
+  $swiperWrapper.insertAdjacentHTML('afterbegin', slides);
 }
 
 const $homeProductListNew = document.querySelector('.home-product-list-new');
